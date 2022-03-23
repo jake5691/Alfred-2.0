512e8e9434ee76ef5a5ca2f27dc318a3935e94af
Jimjams Farm <101795246+jj-jimjams@users.noreply.github.com>
2022-03-23 05:56:50 +0000

    slash options in progress (note options added to static values)

--- modules/specAdvice/cog.py
+++ modules/specAdvice/cog.py
@@ -11,6 +11,7 @@ from functions.greenSpecFunc import *
 from functions.assignSpecFunc import useful_assign, most_use, extra_tile, specAdvice
 from functions import staticValues as sv
 from replit import db
+from nextcord import Interaction, slash_command, Embed, Color, SlashOption, Message
 
 class specAdv(commands.Cog):
   """Fun features"""
@@ -20,10 +21,14 @@ class specAdv(commands.Cog):
 
   @commands.Cog.listener('on_message')
   async def giveAdvice(self,message):
+    #msg = message.content.lower()
+    
     if message.author.bot:
       return
+      
+    #only run in spec advice channel  
     if message.channel.id == sv.channel.skill_point_advice:
-      msg = message.content.lower()
+      
       
       #get language role if any
       target_lang = 'en'
@@ -48,7 +53,7 @@ class specAdv(commands.Cog):
         elif '🇹w' == r.name:
           target_lang = 'zh-CN'
       if message.content.lower().startswith('help'):
-        sendText = "\n\nThis channel provides advice on where to use your specialisation points.\nPlease send your information starting with ADVICE followed by answers to the following questions:\n1. Have you reached your target loyalty?(Y/N)\n2. What is your specialisation level? \n3.Have you already switched to iron/wood tiles? (Y/N)\n4. Are your Frontline Workshops maxxed? (Y/N)\nYour answers should be separated by a space\nFor example: 'Advice N 73 N N'"
+        sendText = "\n\nThis channel provides advice on where to use your specialisation points.\nPlease send your information starting with ADVICE followed by answers to the following questions:\n1. Have you reached your target loyalty?(Y/N)\n2. What is your specialisation level? \n3.Have you already switched to iron/wood tiles? (Y/N)\n4. Are your Frontline Workshops maxxed? (Y/N)\nYour answers should be separated by a space\nFor example: 'Advice N 73 N N'\n\n"
         if target_lang != 'en':
           sendTextTrans = GoogleTranslator(source='auto', target=target_lang).translate(text=sendText)  
         try:
@@ -77,108 +82,87 @@ class specAdv(commands.Cog):
         #except:
          # print('Message could not be deleted')
   
-    #msg = message.content.lower()
-    #print(msg)
-    #if message.author == self.bot.user or message.author.bot:
-       # return
-
-    if len(msg) == 0:
-        return
-    
-    roles = message.author.roles
-    #print(roles)
-    #set variable defaults
-    specadv = "error"
-    loy = "n"
-    spec = 0
-    FullIW = "n"
-    FW = "n"
-
-    #only run in spec advice channel
-    if message.channel.name == 'skill-point-advice':
-
-      #set aliases of advice
-      advice_list = ['advice', 'advise']
-        # set error message
-      errormessage = "Please try again or type HELP for instructions"
-      errormsgtrans = GoogleTranslator(source='auto', target=target_lang).translate(text=errormessage)
-      errormsg = message.channel.send(errormsgtrans)  
-      if not msg.startswith("help"):
-        print("place 1")
-        if not msg.startswith(tuple(advice_list)):
-          msg = GoogleTranslator(source='auto', target='en').translate(text=msg)
-          print("place 2")
-          await errormsg
-
+  #get user input
+  @slash_command(name="specadviceeasy",
+    description="Get advice about where to place your specialisation points",
+    guild_ids=sv.gIDS)
+  async def specAdviceEasy(self, interaction: Interaction,
+    spec:int=SlashOption(
+        name="spec",
+        description="How many specialisation points do you have?",
+        min_value =1,
+        max_value = 170,
+        required=True
+      ),
+    loy:bool=SlashOption(
+        name="loy",
+        description="Have you reached your target loyalty? (True/False)",
+        required=True
+      ),
+    fwmax:bool=SlashOption(
+        name="fwmax",
+        description="Are your Frontline Workshops maxxed?(True/False)",
+        required=True
+      ),
+    fulliw:bool=SlashOption(
+        name="fulliw",
+        description="Are you full on iron and wood tiles?(True/False)",
+        required=True
+      )):
+    """Create a new banner"""
+    #check if command is send in correct channel
+    if not(sv.channel.skill_point_advice == interaction.channel.id):
+      await interaction.response.send_message("Sorry this command can only be used in a specific channel", ephemeral = True)
+      return
+    channel = interaction.channel
+    #Function
+    target_lang = 'en'
+    for r in interaction.user.roles:
+      if '🇩🇪' == r.name:
+        target_lang = 'german'
+      elif '🇪🇸' == r.name:
+        target_lang = 'spanish'
+      elif '🇫🇷' == r.name:
+        target_lang = 'french'
+      elif '🇮🇩' == r.name:
+        target_lang = 'indonesian'
+      elif '🇰🇷' == r.name:
+        target_lang = 'korean'
+      elif '🇳🇱' == r.name:
+        target_lang = 'dutch'
+      elif '🇷🇴' == r.name:
+        target_lang = 'romanian'
+      elif '🇹🇷' == r.name:
+        target_lang = 'turkish'
+      elif '🇹w' == r.name:
+        target_lang = 'zh-CN'
       
+    if loy == False:
+      notes = "Your focus is upgrading CBCs, so you should have 90% food and marble tiles. Depending on the number of resets you have, you will occasionally switch to green left to upgrade Frontline Workshops.\n \n"
+      list1 = ('LoyaltySpeedGroup', 'CBCMat', 'OneExtQ')
+      list2 = ('ExtraTile', 'TileHonour', 'UpgradeBuild')
+    elif fulliw == False:
+      notes = "Your focus is getting high level wood and iron tiles. You may wish to keep a few CBC material tiles if you wish to increase loyalty. You can always take more iron/wood and upgrade later using land development.\n\nIf you think you will fill up on iron/wood before next specialisation reset, ask in alliance chat for advice.\n\n"
+      list1 = ('LoyaltySpeedGroup', 'FWMat', 'OneExtQ')
+      list2 = ('ExtraTile', 'TileHonour', 'TwoExtQs')
+    elif fwmax == False:
+      notes = "Your focus is on upgrading your Frontline Workshops and getting the maximum honour bonus from these upgrades.\n\n"
+      list1 = ('UpgradeBuild', 'FWMat', 'OneExtQ')
+      list2 = ('ExtraTile', 'TileHonour', 'TwoExtQs', 'Land')
+    else:
+      notes = "Your focus is on maximising honour from tiles. You will sometimes switch to green left and extra queues to upgrade Assault and Guardian Fortresses.\n\nIn the last week, you may need to put extra points on processing queues to ensure that you process all of your materials.  Depending on resets you may also prioritise having 49 points on green left (building honour).  Land development and extra tiles might not be necessary.\n\n"
+      list1 = ('TileHonour', 'FWMat', 'ExtraTile')
+      list2 = ('UpgradeBuild', 'TwoExtQs', 'Land')
 
-      if "wife" in msg:
-        await message.channel.send("Getting advice about your wife from a bot will only end in tears.  Your tears."
-            )
-        return
-
-
-      if msg.startswith(tuple(advice_list)):
-          if len(msg) > 7:
-              if msg.split(' ')[1] in ["y", "n"]:
-                  loy = msg.split(' ')[1]
-              else:
-                    #await message.channel.send(random.choice(other_resp))
-                  await errormsg
-                  return
-          if len(msg) > 9:
-            if int(msg.split(' ')[2]) <= 160:
-              userSpecPoints = int(msg.split(' ')[2])
-            else:
-                    #await message.channel.send(random.choice(other_resp))
-              await errormsg
-              await message.channel.send(
-                        "please check your specialisation level is a number less than 160"
-                    )
-              return
-          if len(msg) > 11:
-            if msg.split(' ')[3] in ["y", "n"]:
-                FullIW = msg.split(' ')[3]
-            else:
-                    #await message.channel.send(random.choice(other_resp))
-              await errormsg
-              return
-          if len(msg) > 13:
-            if msg.split(' ')[4] in ["y", "n"]:
-              FW = msg.split(' ')[4]
-            else:
-                    #await message.channel.send(random.choice(other_resp))
-              await errormsg
-              return
-
-          if loy == 'n':
-            notes = "Your focus is upgrading CBCs, so you should have 90% food and marble tiles. Depending on the number of resets you have, you will occasionally switch to green left to upgrade Frontline Workshops.\n \n"
-            list1 = ('LoyaltySpeedGroup', 'CBCMat', 'OneExtQ')
-            list2 = ('ExtraTile', 'TileHonour', 'UpgradeBuild')
-          elif FullIW == 'n':
-            notes = "Your focus is getting high level wood and iron tiles. You may wish to keep a few CBC material tiles if you wish to increase loyalty. You can always take more iron/wood and upgrade later using land development.\n\nIf you think you will fill up on iron/wood before next specialisation reset, ask in alliance chat for advice.\n\n"
-            list1 = ('LoyaltySpeedGroup', 'FWMat', 'OneExtQ')
-            list2 = ('ExtraTile', 'TileHonour', 'TwoExtQs')
-          elif FW == 'n':
-            notes = "Your focus is on upgrading your Frontline Workshops and getting the maximum honour bonus from these upgrades.\n\n"
-            list1 = ('UpgradeBuild', 'FWMat', 'OneExtQ')
-            list2 = ('ExtraTile', 'TileHonour', 'TwoExtQs', 'Land')
-          else:
-        
-            notes = "Your focus is on maximising honour from tiles. You will sometimes switch to green left and extra queues to upgrade Assault and Guardian Fortresses.\n\nIn the last week, you may need to put extra points on processing queues to ensure that you process all of your materials.  Depending on resets you may also prioritise having 49 points on green left (building honour).  Land development and extra tiles might not be necessary.\n\n"
-            list1 = ('TileHonour', 'FWMat', 'ExtraTile')
-            list2 = ('UpgradeBuild', 'TwoExtQs', 'Land')
-          specAdvice(list1, list2,userSpecPoints, groups_bl, groups_gr)
-          await message.channel.send(notes)
-          if target_lang != 'en':
-            notes_trans = GoogleTranslator(
-            source='auto', target=target_lang).translate(text=notes)
-            await message.channel.send(notes_trans)
-        
-          
-  
-          await message.channel.send(file=File('blueSpec.png'))
-          await message.channel.send(file=File('greenSpec.png'))
+      #run spec advice
+      specAdvice(list1, list2, spec, groups_bl, groups_gr)
+      #send advice
+      await channel.send(content =notes)
+      if target_lang != 'en':
+        notes_trans = GoogleTranslator(source='auto', target=target_lang).translate(text=notes)
+      await channel.send(content =notes_trans) 
+      await channel.send(file=File('blueSpec.png'))
+      await channel.send(file=File('greenSpec.png'))
 
 
 def setup(bot: commands.Bot):

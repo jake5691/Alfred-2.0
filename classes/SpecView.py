2cdbec9fc4085e68d05bd3d031a6697a0ea59f0c
Jimjams Farm <101795246+jj-jimjams@users.noreply.github.com>
2022-03-24 10:14:56 +0000

    not working

--- classes/SpecView.py
+++ classes/SpecView.py
@@ -6,6 +6,23 @@ from classes.Member import MemberClass
 from functions import targetFunctions as tf
 
 ##select language first
+class SelectLanguage(Select):
+  """Sets whether the user is a banner castle """
+  def __init__(self):
+    selectOptions = [
+      SelectOption(label='French', description = "bonjour"),
+      SelectOption(label='German', description = "guten tag")
+    ]
+    super().__init__(placeholder = "Are you a banner castle",row=0,min_values=1, max_values=1, options = selectOptions)
+  
+  async def callback(self, interaction:Interaction):
+    
+    self.view.target_lang = self.values[0]
+    print(self.view.target_lang)
+    self.view.whatNext()
+    await interaction.response.edit_message(content=self.view.content, view = self.view)
+
+
 class SelectBanner(Select):
   """Sets whether the user is a banner castle """
   def __init__(self):
@@ -17,8 +34,8 @@ class SelectBanner(Select):
   
   async def callback(self, interaction:Interaction):
     
-    self.banner = self.values[0]
-    print(self.banner)
+    self.view.banner = self.values[0]
+    print(self.view.banner)
     #self.view.whatNext()
     await interaction.response.edit_message(content=f'You selected banner is {self.banner}')
 
@@ -27,19 +44,21 @@ class SpecView(View):
   """The view to hold the Dropdown and Buttons"""
   def __init__(self):
     super().__init__()
-    #self.structures = structures
-    #self.target = Target()
-    #self.targets = targets
-    #self.flags = flags
-    #self.filteredStructures = [s for s in self.structures]
+
+    self.target_lang = 'NotSet'
     self.banner = 'NotSet'
+    self.content = "."
     self.whatNext()
 
   def whatNext(self):
     #self.clear_items()
+    if self.target_lang == 'NotSet':
+      self.add_item(SelectLanguage())
+      print(self.target_lang)
     if self.banner == 'NotSet':
       self.add_item(SelectBanner())
       print(self.banner)
+    
       
       
     

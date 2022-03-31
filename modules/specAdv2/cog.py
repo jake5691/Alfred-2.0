50c445578d7f215a76543479f1297451b7070b5f
Jimjams Farm <101795246+jj-jimjams@users.noreply.github.com>
2022-03-31 09:48:58 +0000

    deletes messages after 5 second delay

--- modules/specAdv2/cog.py
+++ modules/specAdv2/cog.py
@@ -18,7 +18,7 @@ class specAdv2(commands.Cog):
   @slash_command(name="specadvice",
                       description="Press for spec advice.",
                       guild_ids=sv.gIDS)
-  async def specadviceadvanced(self,
+  async def specadvice(self,
       interaction: Interaction):
     """Provides advice on where to place your specialisation points"""
     #Check if advice is asked for in the right channel
@@ -40,7 +40,18 @@ class specAdv2(commands.Cog):
     #waitUntil(view.specinfo.ready == True, specInput(channel, view)) #runs function  
     
     
-    
+  @commands.Cog.listener('on_message')
+  async def delete_messages(self,message):
+    """delete Messages after 5s"""
+    if message.author.bot:
+      return
+    #Loyalty and Skill lvl channel
+    if message.channel.id != sv.channel.skill_point_advice:
+      return
+    try:
+      await message.delete(delay=5)
+    except:
+      print('Message could not be deleted')    
 
 def setup(bot: commands.Bot):
   bot.add_cog(specAdv2(bot))
